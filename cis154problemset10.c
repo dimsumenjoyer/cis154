9e65d50a016b385d923dda5aa528e142f8622ae6
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2024-04-20 20:25:25 -0400

    Add files via upload

--- cis154problemset10.c
+++ cis154problemset10.c
@@ -7,6 +7,8 @@ C - Professor Penta
 */
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 typedef struct
 {
@@ -16,13 +18,12 @@ typedef struct
 
 void printPerson(Person_t* personInfo);
 void happyBirthday(Person_t* personInfo);
-
-// Problem 9.1:
-
-// Problem 9.2:
+int* fillArray(int cardinality);
+void numberFile(int* array);
 
 int main(void)
 {
+    srand(time(NULL));
     Person_t person1 = {"Betsy", 42};
     Person_t person2 = {"Victor", 23};
     printPerson(&person1);
@@ -31,13 +32,32 @@ int main(void)
     happyBirthday(&person2);
 }
 
+// Problem 9.1:
 void printPerson(Person_t* personInfo)
 {
     printf("%s, %d\n", personInfo -> personName, personInfo -> personAge);
 }
 
+// Problem 9.2:
 void happyBirthday(Person_t* personInfo)
 {
     personInfo -> personAge++;
     printf("Happy %d birthday, %s\n", personInfo -> personAge, personInfo -> personName);
+}
+
+// Problem 10.1
+int* fillArray(int cardinality)
+{
+    int* array = (int*)malloc(cardinality * sizeof(int));
+    for (int i = 0; i < cardinality; i++)
+    {
+        int randomNumbers = rand() % (cardinality + 1);
+        array[i] = randomNumbers;
+    }
+    return array;
+}
+
+void numberFile(int* array)
+{
+    FILE *file = fopen("numberFile.md", "w");
 }
